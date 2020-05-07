bb007d31fb0139c708d2ecc428603b93e4eadd75
Justyn Rippie <jrippie@outlook.com>
2020-05-06 22:16:25 -0600

    Javascript shit

---
+++ Brehns Maintance App/wwwroot/js/Sort.js
@@ -0,0 +1,28 @@
+﻿function GetInputsToSort() {
+    alert("button pressed");
+    var amtLow = Number(document.getElementById("priceLow").selectedIndex);
+    var amtHigh = Number(document.getElementById("priceHigh").selectedIndex);
+    sortByPri(amtLow, amtHigh);
+
+ 
+    
+}
+
+function sortByPri(amtLow, amtHigh) {
+    var table, rows, switchen, shouldWeSwitch, row1, row2
+    table = document.getElementById("table")
+    switchen = true;
+
+    while (switchen) {
+        switchen = false;
+        rows = table.rows;
+        for (var i = 0; i < rows.length; i++) {
+            shouldWeSwitch = false;
+            row1 = rows[i].getElementById("amt")[0];
+            row2 = rows[i].getElementById("amt")[0];
+
+            
+
+        }
+    }
+}
