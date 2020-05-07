8f729cf141e3b14aa4d81912783d807448f1594d
Rippie, Justyn <jrippie@abcwua.org>
2020-05-07 11:33:20 -0600

    Javascript things

--- Brehns Maintance App/wwwroot/js/Sort.js
+++ Brehns Maintance App/wwwroot/js/Sort.js
@@ -1,13 +1,29 @@
 ﻿function GetInputsToSort() {
     alert("button pressed");
-    var amtLow = Number(document.getElementById("priceLow").selectedIndex);
-    var amtHigh = Number(document.getElementById("priceHigh").selectedIndex);
-    sortByPri(amtLow, amtHigh);
+    var amtLow;
+    var list1 = document.getElementById("priceLow");
+    var amtHigh; 
+    var list2 = document.getElementById("priceHigh");
 
+    for (var i = 0; i < list1.length; i++) {
+        if (i = document.getElementById("priceLow").selectedIndex) {
+            amtLow = list1[i];
+            break;
+        }
+    }
+
+    for (var i = 0; i < list2.length; i++) {
+        if (i = document.getElementById("priceHigh").selectedIndex) {
+            amtHigh = list2[i];
+            break;
+        }
+    }
+
+    alert("button pressed" + amtHigh + amtLow);
  
     
 }
-
+/*
 function sortByPri(amtLow, amtHigh) {
     var table, rows, switchen, shouldWeSwitch, row1, row2
     table = document.getElementById("table")
@@ -25,4 +41,4 @@ function sortByPri(amtLow, amtHigh) {
 
         }
     }
-}
+}*/
