692d908e5c5905e28a63fbc03440e25238d9f8e1
Rippie, Justyn <jrippie@abcwua.org>
2020-05-11 16:38:17 -0600

    Update to HTML and JS

--- Brehns Maintance App/wwwroot/js/Sort.js
+++ Brehns Maintance App/wwwroot/js/Sort.js
@@ -1,44 +1,47 @@
-﻿function GetInputsToSort() {
-    alert("button pressed");
+﻿function GetInputsToSort()
+{
     var amtLow;
     var list1 = document.getElementById("priceLow");
     var amtHigh; 
     var list2 = document.getElementById("priceHigh");
-
+    
     for (var i = 0; i < list1.length; i++) {
-        if (i = document.getElementById("priceLow").selectedIndex) {
-            amtLow = list1[i];
+        if (i == document.getElementById("priceLow").selectedIndex) {
+            amtLow = list1[i].value;
             break;
         }
     }
 
     for (var i = 0; i < list2.length; i++) {
-        if (i = document.getElementById("priceHigh").selectedIndex) {
-            amtHigh = list2[i];
+        if (i == document.getElementById("priceHigh").selectedIndex) {
+            amtHigh = list2[i].value;
             break;
         }
-    }
+    } 
 
-    alert("button pressed" + amtHigh + amtLow);
- 
-    
+    sortByPri(amtLow, amtHigh);
 }
-/*
-function sortByPri(amtLow, amtHigh) {
-    var table, rows, switchen, shouldWeSwitch, row1, row2
-    table = document.getElementById("table")
-    switchen = true;
 
-    while (switchen) {
-        switchen = false;
-        rows = table.rows;
-        for (var i = 0; i < rows.length; i++) {
-            shouldWeSwitch = false;
-            row1 = rows[i].getElementById("amt")[0];
-            row2 = rows[i].getElementById("amt")[0];
+function sortByPri(amtLow, amtHigh)
+{
+    var table, rows, 
+    table = document.getElementById("table");
+    rows = table.getElementsByTagName("tr");
 
-            
+    for (var i = 1; i < rows.length - 1; i++)
+    {
+        data = rows[i].getElementsByTagName("td")[2];
+        val = data.outerText;
+        val = val.replace("$", "");
+        val = Number(val);
+        amtLow = Number(amtLow);
+        amtHigh = Number(amtHigh);
+        if (val > amtHigh  || val < amtLow ){
 
+            rows[i].style.display = "none";
         }
+        
+
     }
-}*/
+  
+}
